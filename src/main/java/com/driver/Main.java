207812504cdfddc9212e5aa070360336e7adadd6
Sharook <sk.sharook106@gmail.com>
2023-08-09 23:12:27 +0530

    example

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,32 @@
 package com.driver;
 
 public class Main {
+    static class Product
+    {
+        public Product() {
 
+        }
+
+        public int Product(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Product(int x,int y,int z) {
+            return x * y * z;
+        }
+
+        public double Product(double x,double y)
+        {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        System.out.println(p.Product(1,2));
+        System.out.println(p.Product(1,2,3));
+        System.out.println(p.Product(1.1,2.2));
+    }
 }
